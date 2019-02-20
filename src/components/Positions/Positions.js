11e96c68f801940aa4a90535b1b53145364a5a7f
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-02-20 15:07:36 +0600

    added styles

---
+++ src/components/Positions/Positions.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Positions.css";
+
+const Positions = props => {
+    return (
+        <div className="Position">
+            <div onClick={props.onClick}>
+                <img src={props.image} alt=""/>
+                <p>{props.name}</p>
+                <p><b>Price: </b>{props.price} kgs</p>
+            </div>
+        </div>
+    )
+};
+
+export default Positions;
