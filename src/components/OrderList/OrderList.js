0a7bbe8c9c477f2bc5c76b63e35eab54f23f2a03
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-02-20 15:05:43 +0600

    added code and styles

---
+++ src/components/OrderList/OrderList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./OrderList.css";
+
+const Order = props => {
+    return (
+        <div className="Order">
+        <p>{props.initialText}</p>
+    {props.usedPositions.map((ing, key) => {
+        return (
+            <div className="OrderDiv" key={key}>
+            <p>{ing.name}</p>
+        <span>{ing.count} x</span>
+        <span>{ing.price} kgs</span>
+        <button onClick={() => props.remove(key)}>X</button>
+        </div>
+    )
+    })}
+<p className="totalPrice"><b>Total price: </b>{props.total} KGS</p>
+    </div>
+)
+};
+
+export default Order;
