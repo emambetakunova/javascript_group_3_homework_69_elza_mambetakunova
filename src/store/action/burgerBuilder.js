ee3a4501d26b58413410909a807627af42efd644
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-02-22 13:10:31 +0600

    rename file and refactor code

--- src/store/action/burgerBuilder.js
+++
@@ -1,7 +0,0 @@
-import {ADD_INGREDIENT, INIT_INGREDIENTS, REMOVE_INGREDIENT} from "./actionTypes";
-
-export const addIngredient = ingredientName => ({type: ADD_INGREDIENT, ingredientName});
-
-export const removeIngredient = ingredientName => ({type: REMOVE_INGREDIENT, ingredientName});
-
-export const initIngredients = () => ({type: INIT_INGREDIENTS});
